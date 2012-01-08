25c283a31a31081a17d9001c2d5b414bb3082f5d
Aaron Alexander <drunknbass@me.com>
2012-01-08 10:14:19 -0800

    added missing files

---
+++ DNBSwipyNavigationController/UIViewController+SwipyAdditions.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+SideBarAdditions.h
+//  DeckControllerSample
+//
+//  Created by Aaron Alexander on 12/7/11.
+//  Copyright (c) 2011 drunknbass. All rights reserved.
+//
+
+#import "AFSideBarItem.h"
+#import "AFSideBarController.h"
+
+@interface UIViewController (SwipyAdditions)
+
+@property (nonatomic, retain) UIViewController *leftController;
+@property (nonatomic, retain) UIViewController *rightController;
+
+@end
